7b30fcbe3bd115d1e60b6c768e864eceb88e0e60
HernandoMV <hernybiotec@gmail.com>
2025-01-23 17:59:14 +0100

    .

--- playground.py
+++ playground.py
@@ -1,62 +1,44 @@
 # %%
 import numpy as np
+import pandas as pd
 from sound_functions import cloud_of_tones
 # %%
-
-
+lowest_freq = 5000
+highest_freq = 20000
+freqs_log_spaced = np.round(np.logspace(np.log10(lowest_freq), np.log10(highest_freq), 18)).tolist()
+low_freqs = freqs_log_spaced[:6]
+high_freqs = freqs_log_spaced[-6:]
+# Define sound properties
 sound_properties = {
     "sample_rate": 48000,
-    "duration": 1,
+    "duration": .5,
     "ramp_time": 0.005,
-    "amplitude": 0.02,
-    "high_freq": np.round(np.logspace(np.log10(11000), np.log10(20000), 16)).tolist(),
-    "low_freq": np.round(np.logspace(np.log10(5000), np.log10(8000), 16)).tolist(),
+    "high_amplitude_mean": 60,
+    "low_amplitude_mean": 70,
+    "amplitude_std": 2,
+    "high_freq_list": high_freqs,
+    "low_freq_list": low_freqs,
     "subduration": 0.03,
     "suboverlap": 0.01,
 }
-
-
-# Generate a cloud of tones
-sound, frequencies = cloud_of_tones(**sound_properties, high_perc=95, low_perc=5)
-
-# # print the percentage of high and low tones
-print("High tones: ", np.sum(frequencies[1] > 0) / len(frequencies[1]) * 100)
-# # print all frequencies different from 0
-# h_t = frequencies[1][frequencies[1] > 0]
-# print(h_t)
-print("Low tones: ", np.sum(frequencies[0] > 0) / len(frequencies[0]) * 100)
-# l_t = frequencies[0][frequencies[0] > 0]
-# print(l_t)
-
-# plot a spectrogram
-import matplotlib.pyplot as plt
-from scipy.signal import spectrogram
-
-f, t, Sxx = spectrogram(sound, sound_properties["sample_rate"])
-plt.pcolormesh(t, f, 10 * np.log10(Sxx))
-plt.ylabel("Frequency [Hz]")
-plt.xlabel("Time [sec]")
-plt.ylim(0, 20000)  # limit the y axis to 20 kHz
-# y axis log
-plt.show()
+# fix the random seed
+np.random.seed(0)
 
 # %%
-np.round(np.logspace(np.log10(5000), np.log10(20000), 18)).tolist()[:6]
-# %%
-# create a sample dataframe
-import pandas as pd
-
-sample_df = pd.DataFrame(
-    {
-        "A": [1, 2, 3, 4, 5],
-        "B": [6, 7, 8, 9, 10],
-        "C": [11, 12, 13, 14, 15],
-    }
-)
-sample_df
+cot, high_mat, low_mat = cloud_of_tones(
+    **sound_properties,
+    high_prob=0.7,
+    low_prob=0.3,
+    )
 
 # %%
-sample_df.to_dict(orient="records")
+storage = {
+    "high_tones": high_mat.to_dict(),
+    "low_tones": low_mat.to_dict(),
+}
+
+# %%
+pd.DataFrame(storage["high_tones"])
 # %%
 from bitarray import bitarray
 
