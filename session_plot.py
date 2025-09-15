8b5df1ae51e6f00b42447ec9273dd0029038dee8
HernandoMV <hvergarabio@gmail.com>
2025-09-15 11:21:03 +0200

    .

--- session_plot.py
+++ session_plot.py
@@ -8,9 +8,9 @@ class Session_Plot(SessionPlotFigureManager):
     def __init__(self) -> None:
         super().__init__()
 
-    def create_plot(self, df: pd.DataFrame, df_raw: pd.DataFrame, width: float = 10, height: float = 8) -> Figure:
+    def create_plot(self, df: pd.DataFrame, df_raw: pd.DataFrame, width: float = 15, height: float = 8) -> Figure:
         # add a dummy session column to the df
         df['session'] = 1
         # get the name of the mouse
         mouse_name = df.subject.iloc[0]
-        return session_summary_figure(df, mouse_name=mouse_name, width=width, height=height)
+        return session_summary_figure(df, mouse_name=mouse_name, width=height, height=width)
